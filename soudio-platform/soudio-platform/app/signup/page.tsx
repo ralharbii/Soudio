a8803aee647692be8d470cf3c22c4420b0fec00e
ralharbii <ralharbi.aa@gmail.com>
2026-03-05 15:50:20 +0300

    Delete soudio-platform directory

--- soudio-platform/soudio-platform/app/signup/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-// Redirect to login page with signup tab
-export default function SignupPage() {
-  redirect("/login");
-}
--- soudio-platform/soudio-platform/components/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export { Button } from "./Button";
-export { Card, CardHeader, CardBody, CardFooter } from "./Card";
-export { Container } from "./Container";
-export { Section, SectionHeading } from "./Section";
